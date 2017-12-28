5f772efefd79c0f599d4f8a4ed8f16abaaccbc6c
Unknown <mario.lengauer@gmx.net>
2017-12-28 15:42:29 +0100

    Add File

---
+++ navbar.php
@@ -0,0 +1,34 @@
+ <nav class="navbar is-dark">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="Home.php">
+        <img src="LOGO.png" alt="Logo" width="112" height="28">
+    </a>
+    <button class="navbar-burger burger" data-target="navMenuColordark" style="background-color: hsla(210, 100%, 50%, 0.0); border: none;">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  </div>
+
+  <div id="navMenuColordark" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="Home.php">
+        Home
+      </a>
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">Reservierung</a>
+      </div>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field is-grouped">
+          <p class="control">
+            <a class="button is-primary" href="logout.php">
+              <span>Abmelden</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
