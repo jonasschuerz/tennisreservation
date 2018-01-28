8b77729a8232b3b87bd34af2359bda8a830732fa
Unknown <mario.lengauer@gmx.net>
2018-01-28 21:10:01 +0100

    logout add

---
+++ logout.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html style="height: 100%; background-color: #606060;">
+<head>
+  <meta charset="utf-8">
+  <title>Logout</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
+</head>
+<body>
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Logout erfolgreich</p>
+        <button class="delete" aria-label="close"></button>
+      </header>
+      <footer class="modal-card-foot">
+        <a class="button is-info" href="login.php">Zurück zum Login</a>
+      </footer>
+    </div>
+  </div>
+</body>
+</html>
