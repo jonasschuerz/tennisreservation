1b7928bec0cf05ae5ff26c36e0ec1a50a8b05ae9
jonasschuerz <32167805+jonasschuerz@users.noreply.github.com>
2018-02-27 12:40:14 +0100

    Update context.php

--- context.php
+++ context.php
@@ -24,13 +24,6 @@
 
     <script src="js/Script.js"></script>
     <link rel="stylesheet" type="text/css" href="css/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-    <link rel="stylesheet" type="text/css" href="css/vendor/animate/animate.css">
-    <link rel="stylesheet" type="text/css" href="css/vendor/css-hamburgers/hamburgers.min.css">
-    <link rel="stylesheet" type="text/css" href="css/vendor/animsition/css/animsition.min.css">
-    <link rel="stylesheet" type="text/css" href="css/vendor/select2/select2.min.css">
-    <link rel="stylesheet" type="text/css" href="css/vendor/daterangepicker/daterangepicker.css">
     <link rel="stylesheet" type="text/css" href="css/util.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
