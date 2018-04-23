a5cec6a445cec609948e297db583c25821253ee7
Unknown <mario.lengauer@gmx.net>
2018-04-23 10:34:05 +0200

    update

--- handlelogout.php
+++ handlelogout.php
@@ -2,5 +2,6 @@
     require('AccountSession.php');
     if(AccountSession::logout()){
         echo json_encode(array("error"=>false, "description"=>"Logout erfolgreich"));
+        die();
     }
     echo json_encode(array("error"=>true, "description"=>"Logout fehlgeschlagen"));
